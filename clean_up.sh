d3f2df01ce09c4e3e7e46ad9aaa8feded7383e4b
Hadi Alqattan <alqattanhadizaki@gmail.com>
2021-04-09 22:13:04 +0300

    Update: clean up script, remove local&remote tags.

--- clean_up.sh
+++ clean_up.sh
@@ -2,8 +2,18 @@
 
 set -e
 
-GLOBIGNORE=LICENSE:clean_up.sh:.git
+# Remove all files/directories except .git/, LICENSE, and clean_up.sh.
+GLOBIGNORE=.git:LICENSE:clean_up.sh
 
 rm -fr *
 
 unset GLOBIGNORE
+
+# Remove both local & remote tags.
+git tag -d $(git tag -l | head -n 100)
+
+git fetch
+
+git push origin --delete $(git tag -l)
+
+git tag -d $(git tag -l | head -n 100)
