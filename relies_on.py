56656a46ff56343d88d127a554e281724c1d20d3
Hadi Alqattan <alqattanhadizaki@gmail.com>
2022-04-01 23:41:58 +0300

    baby shark do do do do.

--- relies_on.py
+++ relies_on.py
@@ -259,15 +259,12 @@ def str2bool(val: str) -> bool:
 
 
 def main() -> int:  # pylint: disable=missing-function-docstring
-    print(f"GITHUB_REPOSITORY={os.getenv('GITHUB_REPOSITORY', '')}")
-    github_env_file = os.getenv("GITHUB_ENV", "")
-    with open(github_env_file, "r") as gh_env:
-        print(gh_env.read())
+    owner, repo = os.getenv("GITHUB_REPOSITORY", "").split("/")
     filter_ = Filter(
-        owner=os.getenv("INPUT_OWNER", "").lower(),
-        repo=os.getenv("INPUT_REPOSITORY", "").lower(),
-        workflow_name=os.getenv("INPUT_WORKFLOW", "").lower(),
-        branch=os.getenv("INPUT_BRANCH", "").lower(),
+        owner=os.getenv("INPUT_OWNER", owner),
+        repo=os.getenv("INPUT_REPOSITORY", repo),
+        workflow_name=os.getenv("INPUT_WORKFLOW", ""),
+        branch=os.getenv("INPUT_BRANCH", ""),
         event=os.getenv("INPUT_EVENT", "").lower(),
         exclude_pull_requests=str2bool(
             os.getenv("INPUT_EXCLUDE_PULL_REQUESTS", "true")
